f4a22765d3d03e750e12490fe2832221e54cea52
George W Langham (desktop) <georgelangham1990@gmail.com>
2020-12-07 23:30:21 +0000

    some basic structure. add dayjs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,46 @@
-import { useEffect, useState } from "react";
-
+import { useEffect, useState } from 'react';
 const getData = async () =>
   await fetch('/api/posts').then((response) => response.json());
-
 function App() {
-  const [posts,setPosts]=useState();
-  useEffect(()=>{
+  const [posts, setPosts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  useEffect(() => {
     async function fetchData() {
-      if(!posts){
-        const fetchedData = await getData().catch((err)=>console.error('Failed to Fetch',err))
-        if(fetchedData?.posts){
+      if (posts?.length <= 0) {
+        const fetchedData = await getData().catch((err) =>
+          console.error('Failed to Fetch', err)
+        );
+        if (fetchedData?.posts) {
           setPosts(fetchedData.posts);
         }
-      }      
+      }
     }
     fetchData();
-  },[posts])
-  return <div>
-    <ul>
-    {posts.length>0?
-    posts?.map(post=><>{JSON.stringify(post)}</>):
-    <li>No Posts</li>}
-    </ul>
-  </div>;
+  }, [posts]);
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1>Posts</h1>
+        <ul>
+          {posts.length > 0 ? (
+            posts?.map((post) => (
+              <li key={post.id} className={styles.row}>
+                <h2>{post.title}</h2>
+                <div></div>
+                {JSON.stringify(Object.keys(post))}
+              </li>
+            ))
+          ) : (
+            <li>No Posts</li>
+          )}
+        </ul>
+      </main>
+      <footer>
+        GeorgeWL
+      </footer>
+    </>
+  );
 }
 
 export default App;
