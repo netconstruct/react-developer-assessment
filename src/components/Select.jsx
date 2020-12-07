b3d1d4cce287647a6690f5796158b37d198add52
George W Langham (desktop) <georgelangham1990@gmail.com>
2020-12-07 23:33:18 +0000

    init select input

---
+++ src/components/Select.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+const Select = ({
+  options = [],
+  onChange,
+  value,
+  hasEmptyValue = true,
+  multi = false,
+  id='selectInput'
+}) => (
+  <select id={id} onChange={onChange} value={value} multi={multi}>
+    {hasEmptyValue && <option disabled={true}>Please Select</option>}
+    {options?.map((opt) => {
+      <option key={opt.value} value={opt.value}>{opt.label}</option>;
+    })}
+  </select>
+);
+Select.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({ label: PropTypes.node, value: PropTypes.string })
+  ).isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  id:PropTypes.string,
+  hasEmptyValue: PropTypes.bool,
+  multi: PropTypes.bool,
+};
