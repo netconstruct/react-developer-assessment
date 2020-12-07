b567f977442ff03a9001331cf95f669d7bee0618
George W Langham (desktop) <georgelangham1990@gmail.com>
2020-12-07 22:40:31 +0000

    init fetching

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
+const getData = async () =>
+  await fetch('/api/posts').then((response) => response.json());
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  const [posts,setPosts]=useState();
+  useEffect(()=>{
+    async function fetchData() {
+      if(!posts){
+        const fetchedData = await getData().catch((err)=>console.error('Failed to Fetch',err))
+        if(fetchedData?.posts){
+          setPosts(fetchedData.posts);
+        }
+      }      
+    }
+    fetchData();
+  },[posts])
+  return <div></div>;
 }
 
 export default App;
