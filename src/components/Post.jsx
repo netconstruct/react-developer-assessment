ee2c5ae5ca7d399ddf656fe47f7693300f7b84ba
George W Langham (desktop) <georgelangham1990@gmail.com>
2020-12-08 17:03:18 +0000

    basic stylings

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,28 +2,27 @@ import { Link } from 'react-router-dom';
 import { formatDateString } from '../helpers/formatters';
 import AuthorLabel from './AuthorLabel';
 import CategoriesList from './CategoriesList';
-
-const Post = ({ post, isDetail }) =>  (
-    <Wrapper isDetail={isDetail}>
+import styles from '../styles/post.module.scss';
+const Post = ({ post, isDetail }) => (
+  <Wrapper isDetail={isDetail} className={styles.wrapper}>
+    <div className={styles.main}>
+      <h2>{post.title}</h2>
+      <AuthorLabel name={post.author.name} avatar={post.author.avatar} />
       <div>
-        <h2>{post.title}</h2>
-        <AuthorLabel name={post.author.name} avatar={post.author.avatar} />
-        <div>
-          <p>
-            {post.summary
-              .slice(
-                0,
-                !isDetail && post.summary.length > 130
-                  ? 130
-                  : post.summary.length
-              )
-              .trim()}
-            {!isDetail && post.summary.length > 130 && '...'}
-          </p>
-        </div>
-        <div>
-          <p>{formatDateString(post.publishDate)}</p>
-        </div>
+        <p>
+          {post.summary
+            .slice(
+              0,
+              !isDetail && post.summary.length > 130 ? 130 : post.summary.length
+            )
+            .trim()}
+          {!isDetail && post.summary.length > 130 && '...'}
+        </p>
+      </div>
+      <div>
+        <p>{formatDateString(post.publishDate)}</p>
+      </div>
+      <div className={styles.postFooter}>
         <CategoriesList categories={post.categories} />
         {isDetail ? (
           <Link to="/">Back To Posts</Link>
@@ -31,9 +30,16 @@ const Post = ({ post, isDetail }) =>  (
           <Link to={`/post/${post.id}`}>Read more</Link>
         )}
       </div>
-    </Wrapper>
-  );
+    </div>
+  </Wrapper>
+);
 
-const Wrapper = ({ children, isDetail }) =>
-  isDetail ? <section id="post">{children}</section> : <li>{children}</li>;
+const Wrapper = ({ children, isDetail, className }) =>
+  isDetail ? (
+    <section className={className} id="post">
+      {children}
+    </section>
+  ) : (
+    <li className={className}>{children}</li>
+  );
 export default Post;
