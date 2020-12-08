7aebb19789bbc4891de4171c91c618031e2d0191
George W Langham (desktop) <georgelangham1990@gmail.com>
2020-12-08 02:04:10 +0000

    category flattener

--- src/helpers/formatters.js
+++ src/helpers/formatters.js
@@ -1,6 +1,16 @@
 import dayjs from 'dayjs';
 import localisedFormat from 'dayjs/plugin/localizedFormat';
 dayjs.extend(localisedFormat);
+
 export function formatDateString(dateString) {
     return dayjs(dateString).format('DD/MM/YYYY LT');
+}
+export function getCategoriesFromData(data){
+    const categories = []
+    data.forEach(entry=>{
+        categories.push(entry.categories.map(category=>category.name))
+    })
+    const flattenedCategories = categories.flat();
+    const result = [...new Set(flattenedCategories)]
+    return result
 }
