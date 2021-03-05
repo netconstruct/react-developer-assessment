caeda0bd28e7a0d95b4423eac65d95e1c4791378
Pedro Klepa <pedro.klepa@gmail.com>
2021-03-05 14:53:11 +0000

    Basic API fetch and display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/posts')
+      .then((response) => response.json())
+      .then((res) => setPosts(res.posts));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li>{post.author.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
