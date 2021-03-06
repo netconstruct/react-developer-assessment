e114d173d02d184052e3bd1fefbb4e1b03e185ca
Pedro Klepa <pedro.klepa@gmail.com>
2021-03-06 10:38:47 +0000

    Categories List now consists of a list of buttons that can toggle the display of posts with said catogory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,18 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
   const [categories, setCategories] = useState([]);
-  // const [postsToDisplay, setPostsToDisplay] = useState([]);
-  // const [activeCategory, setActiveCategory] = useState([]);
+  const [postsToDisplay, setPostsToDisplay] = useState([]);
+  const [activeCategories, setActiveCategories] = useState([]);
 
-  const POSTS_PER_PAGE = 6;
+  const POSTS_PER_PAGE = 3;
 
-  // function filterByCategory(postList, category) {
-  //   if (category === 'ALL') return postList;
+  function filterByCategory(postList, categoriesArr) {
+    if (categoriesArr.length === 0) return postList;
 
-  //   return postList.filter((post) =>
-  //     post.categories.some((cat) => cat.name === category)
-  //   );
-  // }
+    return postList.filter(({ categories }) =>
+      categories.some((category) => categoriesArr.includes(category.name))
+    );
+  }
 
   function getAllCategories(postList) {
     const categoriesNamesArr = [];
@@ -30,6 +30,17 @@ function App() {
     return categoriesNamesArr;
   }
 
+  function toggleCategory(category) {
+    if (!activeCategories.includes(category))
+      return setActiveCategories([...activeCategories, category]);
+
+    const categoryIndex = activeCategories.indexOf(category);
+    const newActiveCategories = [...activeCategories];
+    newActiveCategories.splice(categoryIndex, 1);
+
+    return setActiveCategories(newActiveCategories);
+  }
+
   // TODO: Improve error handling
   useEffect(() => {
     fetch('/api/posts')
@@ -39,14 +50,16 @@ function App() {
   }, []);
 
   useEffect(() => {
+    setPostsToDisplay(filterByCategory(posts, activeCategories));
     setCategories(getAllCategories(posts));
-  }, [posts]);
+  }, [posts, activeCategories]);
 
   return (
     <div>
       <h1>POSTS</h1>
       <ul>
-        {posts.map((post, index) => {
+        {postsToDisplay.length}
+        {postsToDisplay.map((post, index) => {
           if (index + 1 > page * POSTS_PER_PAGE) return null;
 
           return <li key={post.id}> {post.author.name}</li>;
@@ -60,7 +73,13 @@ function App() {
       <h1>CATEGORIES</h1>
       <ul>
         {categories.map((category) => {
-          return <li key={category}> {category}</li>;
+          return (
+            <li key={category}>
+              <button onClick={() => toggleCategory(category)}>
+                {category}
+              </button>
+            </li>
+          );
         })}
       </ul>
     </div>
