f21654b6c92fd1f4caabea9ad315369bf4bfffa4
Simona Cartuta <simonacartuta1@gmail.com>
2021-04-10 15:46:06 +0100

    Fetched posts data and created interfaces for the data
    Created the interfaces to have more control over types while developing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { IPost } from "../utils/IPost";
 
 const App = () => {
-  return <div>Created initial develop branch</div>;
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    fetch('api/posts')
+      .then(response => response.json())
+      .then(results => setPosts(results.posts))
+      .catch(error => console.error(`Oops, could not retrieve data. Please, try again later. \nError: ${error}`));
+  }, []);
+
+  return <div>{/* Complete the exercise here. */}</div>;
 }
 
 export default App;
